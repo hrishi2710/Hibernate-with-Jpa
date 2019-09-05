1b19deb4d3ef3d1563deac6cfa499db0f33eb62a
hrishi2710 <hrishirules2710@gmail.com>
2019-09-05 14:08:37 +0530

    Implemented table per class

--- demo/src/main/java/org/hrishi/jpa/hibernate/demo/entity/Employee.java
+++ demo/src/main/java/org/hrishi/jpa/hibernate/demo/entity/Employee.java
@@ -1,7 +1,6 @@
 package org.hrishi.jpa.hibernate.demo.entity;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,8 +8,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "Employee_type")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Employee {
 	
 	@Id
